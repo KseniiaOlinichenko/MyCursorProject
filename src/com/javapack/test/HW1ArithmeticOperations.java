fadc52501a3b0e1e37b06fe2d11b1acc1a25c3b9
KseniiaOlinichenko <olinichenkooo@gmail.com>
2023-03-01 23:24:05 +0300

    Arithmetic Operations with primitives

---
+++ src/com/javapack/test/HW1ArithmeticOperations.java
@@ -0,0 +1,18 @@
+package com.javapack.test;
+
+public class HW1ArithmeticOperations {
+    public static void main(String[] args) {
+        byte b = (-6);
+        short s = 2736;
+        long l = 283792;
+        float f = (-763.87f);
+        double d = 827381.44;
+
+        System.out.println("Result of adding: " + (b + f));
+        System.out.println("Result of multiplying: " + (s * l));
+        System.out.println("Result of subtracting: " + (d - l));
+        System.out.println("Result of dividing: " + (b / f));
+
+
+    }
+}
